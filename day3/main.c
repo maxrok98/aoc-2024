629ae3ad07f98583db7d6ebbf7f53885092dce68
maxrok98 <max.rok.98@gmail.com>
2024-12-03 15:31:04 +0200

    day 3 part 1

---
+++ day3/main.c
@@ -0,0 +1,128 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <assert.h>
+
+int LoadFile(char* fileName, char** content) {
+	FILE* f = fopen(fileName, "rb");
+	if(f == NULL) {
+		printf("Error: %s file not found!\n", fileName);
+		exit(1);
+	}
+	fseek(f, 0, SEEK_END);
+	long fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char* fileContent = malloc(fsize + 1);
+	fread(fileContent, fsize, 1, f);
+	fclose(f);
+
+	fileContent[fsize] = 0;
+	*content = fileContent;
+	return fsize;;
+}
+
+typedef enum TokenType {
+	L_PAREN,
+	R_PAREN,
+	INTEGER,
+	COMMA,
+	MUL,
+	GARBAGE,
+	END
+} TokenType;
+
+typedef struct Token {
+	TokenType tokenType;
+	int value;
+} Token;
+
+typedef struct {
+	char* string;
+	int length;
+
+	/* window indexes */
+	int start;
+	int end;
+} Tokenizer;
+
+void AdvanceCursor(Tokenizer* tokenizer) {
+	tokenizer->start = ++tokenizer->end;
+}
+
+void SetCursor(Tokenizer* tokenizer, int num) {
+	tokenizer->start = tokenizer->end = num;
+}
+
+Token nextMulToken(Tokenizer* tokenizer) {
+	assert(tokenizer->start == tokenizer->end);
+	Token token;
+	while(true) {
+		while(tokenizer->string[tokenizer->start] != 'm' && tokenizer->start < tokenizer->length) {
+			AdvanceCursor(tokenizer);
+		}
+		if(tokenizer->start >= tokenizer->length-2) break;
+		if(tokenizer->string[tokenizer->end+1] != 'u') { AdvanceCursor(tokenizer); continue; }
+		if(tokenizer->string[tokenizer->end+2] != 'l') { AdvanceCursor(tokenizer); continue; }
+		SetCursor(tokenizer, tokenizer->end+3);
+		return token = (Token) { .tokenType = MUL };
+	}
+	return token = (Token) { .tokenType = END };
+}
+
+int ToInteger(char* string, int start, int end) {
+	int num = 0;
+	for(int i = start; i <= end; i++) {
+		num = num * 10 + (string[i] - 48);
+	}
+	return num;
+}
+
+Token nextToken(Tokenizer* tokenizer) {
+	assert(tokenizer->start == tokenizer->end);
+	Token token;
+	while(true) {
+		if(tokenizer->start >= tokenizer->length) { token = (Token) { .tokenType = END }; break; }
+		if(tokenizer->string[tokenizer->start] == '(') { token = (Token) { .tokenType = L_PAREN }; break; }
+		if(tokenizer->string[tokenizer->start] == ')') { token = (Token) { .tokenType = R_PAREN }; break; }
+		if(tokenizer->string[tokenizer->start] == ',') { token = (Token) { .tokenType = COMMA   }; break; }
+		if(isdigit(tokenizer->string[tokenizer->start])) {
+			while (isdigit(tokenizer->string[tokenizer->end])) { tokenizer->end++; }
+			tokenizer->end--;
+			int num = ToInteger(tokenizer->string, tokenizer->start, tokenizer->end);
+			token = (Token) { .tokenType = INTEGER, .value = num }; break;
+		}
+		token = (Token) { .tokenType = GARBAGE }; break;
+	}
+	AdvanceCursor(tokenizer);
+	return token;
+}
+
+int main() {
+	char* fileContent;
+	int length = LoadFile("input.txt", &fileContent);
+
+	Tokenizer tokenizer = {
+		.string = fileContent,
+		.length = length,
+		.start = 0,
+		.end = 0
+	};
+
+	int sum = 0;
+	while(nextMulToken(&tokenizer).tokenType != END) {
+		Token num1;
+		Token num2;
+		if(nextToken(&tokenizer).tokenType != L_PAREN) continue;
+		if((num1 = nextToken(&tokenizer)).tokenType != INTEGER) continue;
+		if(nextToken(&tokenizer).tokenType != COMMA) continue;
+		if((num2 = nextToken(&tokenizer)).tokenType != INTEGER) continue;
+		if(nextToken(&tokenizer).tokenType != R_PAREN) continue;
+		sum += num1.value * num2.value;
+	}
+
+	printf("Part 1: %d\n", sum);
+	return 0;
+}
