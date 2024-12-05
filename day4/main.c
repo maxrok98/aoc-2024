b6ed060f98b0a4582e5acaf0ab90b26ce19e938d
maxrok98 <max.rok.98@gmail.com>
2024-12-05 13:47:38 +0200

    refactored day 4

--- day4/main.c
+++ day4/main.c
@@ -8,6 +8,7 @@
 #define HEIGHT 140
 #define WIDTH 140
 
+char letters[HEIGHT][WIDTH];
 char* XMAS = "XMAS";
 char* MAS = "MAS";
 
@@ -40,112 +41,52 @@ void ParseFile(char* fileName, char* letters) {
 	fclose(f);
 }
 
-char letter(char* letters, int i, int j) {
-	return *(letters+(i*WIDTH)+j);
-}
-
-int SearchXmas(char* letters, int i, int j, int letterNum, Direction direction) {
+int SearchXmas(int i, int j, int letterNum, Direction direction) {
 	int valid_top = (i != 0), valid_bottom = (i != HEIGHT-1);
 	int valid_left = (j != 0), valid_right = (j != WIDTH-1); 
 
 	int sum = 0;
 	if(XMAS[letterNum] == 'S') 				return 1;
 
-	if(valid_top && valid_left && 	  (direction == NO || direction == TOP_LEFT		 ) &&	letter(letters, i-1, j-1) == XMAS[letterNum+1])	sum += SearchXmas(letters, i-1, j-1, letterNum+1, direction == NO ? TOP_LEFT 		 : direction);
-	if(valid_top && 								  (direction == NO || direction == TOP		 		 ) && letter(letters, i-1, j  ) == XMAS[letterNum+1])	sum += SearchXmas(letters, i-1, j,   letterNum+1, direction == NO ? TOP			 		 : direction);
-	if(valid_top && valid_right && 	  (direction == NO || direction == TOP_RIGHT	 ) && letter(letters, i-1, j+1) == XMAS[letterNum+1])	sum += SearchXmas(letters, i-1, j+1, letterNum+1, direction == NO ? TOP_RIGHT 	 : direction);
-	if(valid_left && 								  (direction == NO || direction == LEFT				 ) && letter(letters, i,   j-1) == XMAS[letterNum+1])	sum += SearchXmas(letters, i,   j-1, letterNum+1, direction == NO ? LEFT 				 : direction);
-	if(valid_right && 							  (direction == NO || direction == RIGHT			 ) && letter(letters, i,   j+1) == XMAS[letterNum+1])	sum += SearchXmas(letters, i,   j+1, letterNum+1, direction == NO ? RIGHT 			 : direction);
-	if(valid_bottom && valid_left &&  (direction == NO || direction == BOTTOM_LEFT ) &&	letter(letters, i+1, j-1) == XMAS[letterNum+1])	sum += SearchXmas(letters, i+1, j-1, letterNum+1, direction == NO ? BOTTOM_LEFT  : direction);
-	if(valid_bottom && 							  (direction == NO || direction == BOTTOM			 ) && letter(letters, i+1, j  ) == XMAS[letterNum+1])	sum += SearchXmas(letters, i+1, j,   letterNum+1, direction == NO ? BOTTOM 			 : direction);
-	if(valid_bottom && valid_right && (direction == NO || direction == BOTTOM_RIGHT) && letter(letters, i+1, j+1) == XMAS[letterNum+1]) sum += SearchXmas(letters, i+1, j+1, letterNum+1, direction == NO ? BOTTOM_RIGHT : direction);
+	if(valid_top && valid_left && 	  (direction == NO || direction == TOP_LEFT		 ) &&	letters[i-1][j-1] == XMAS[letterNum+1])	sum += SearchXmas(i-1, j-1, letterNum+1, direction == NO ? TOP_LEFT 		: direction);
+	if(valid_top && 								  (direction == NO || direction == TOP		 		 ) && letters[i-1][j  ] == XMAS[letterNum+1])	sum += SearchXmas(i-1, j,   letterNum+1, direction == NO ? TOP			 		: direction);
+	if(valid_top && valid_right && 	  (direction == NO || direction == TOP_RIGHT	 ) && letters[i-1][j+1] == XMAS[letterNum+1])	sum += SearchXmas(i-1, j+1, letterNum+1, direction == NO ? TOP_RIGHT 	  : direction);
+	if(valid_left && 								  (direction == NO || direction == LEFT				 ) && letters[i  ][j-1] == XMAS[letterNum+1])	sum += SearchXmas(i,   j-1, letterNum+1, direction == NO ? LEFT 				: direction);
+	if(valid_right && 							  (direction == NO || direction == RIGHT			 ) && letters[i  ][j+1] == XMAS[letterNum+1])	sum += SearchXmas(i,   j+1, letterNum+1, direction == NO ? RIGHT 			  : direction);
+	if(valid_bottom && valid_left &&  (direction == NO || direction == BOTTOM_LEFT ) &&	letters[i+1][j-1] == XMAS[letterNum+1])	sum += SearchXmas(i+1, j-1, letterNum+1, direction == NO ? BOTTOM_LEFT  : direction);
+	if(valid_bottom && 							  (direction == NO || direction == BOTTOM			 ) && letters[i+1][j  ] == XMAS[letterNum+1])	sum += SearchXmas(i+1, j,   letterNum+1, direction == NO ? BOTTOM 			: direction);
+	if(valid_bottom && valid_right && (direction == NO || direction == BOTTOM_RIGHT) && letters[i+1][j+1] == XMAS[letterNum+1]) sum += SearchXmas(i+1, j+1, letterNum+1, direction == NO ? BOTTOM_RIGHT : direction);
 
 	return sum;
 }
 
-typedef struct {
-	int i;
-	int j;
-} Point;
-
-typedef struct {
-	Point* points;
-	int count;
-	int allocated;
-} PointArray;
-PointArray pointArray;
-
-void InitPointArray(PointArray* pointArray) {
-	pointArray->count = 0;
-	pointArray->allocated = ARRAY_LENGTH;
-	pointArray->points = (Point*)calloc(ARRAY_LENGTH, sizeof(Point));
-}
-
-void AddPoint(PointArray* pointArray, Point point) {
-	if(pointArray->count == pointArray->allocated) {
-		pointArray->points = (Point*)realloc(pointArray->points, sizeof(Point)*(pointArray->allocated*=2));
-	}
-	pointArray->points[pointArray->count++] = point;
-}
-
-bool ContainsPoint(PointArray* pointArray, Point point) {
-	for(int i = 0; i < pointArray->count; i++) {
-		if(point.i == pointArray->points[i].i && point.j == pointArray->points[i].j) return true;
-	}
-	return false;
-}
-
-int VerifyMas(char* letters, int i, int j, Direction direction) {
-	assert(letter(letters, i, j) == 'A');
+int VerifyMas(int i, int j, Direction direction) {
+	assert(letters[i][j] == 'A');
 	int valid_top = (i != 0), valid_bottom = (i != HEIGHT-1);
 	int valid_left = (j != 0), valid_right = (j != WIDTH-1); 
 	if(!(valid_top && valid_bottom && valid_left && valid_right)) return 0;
 
-	if(direction == TOP_LEFT &&	letter(letters, i-1, j-1) == 'S') {
-		if(letter(letters, i+1, j-1) == 'M' && letter(letters, i-1, j+1) == 'S') {
-			Point point = { .i = i+1, .j = j - 1};
-			//AddPoint(&pointArray, point);
-			return 1;
-		}
-		if(letter(letters, i+1, j-1) == 'S' && letter(letters, i-1, j+1) == 'M') {
-			Point point = { .i = i-1, .j = j+1};
-			//AddPoint(&pointArray, point);
+	if(direction == TOP_LEFT &&	letters[i-1][j-1] == 'S') {
+		if((letters[i+1][j-1] == 'M' && letters[i-1][j+1] == 'S') ||
+		   (letters[i+1][j-1] == 'S' && letters[i-1][j+1] == 'M')) {
 			return 1;
 		}
 	}
-	if(direction == TOP_RIGHT &&	letter(letters, i-1, j+1) == 'S') {
-		if(letter(letters, i-1, j-1) == 'M' && letter(letters, i+1, j+1) == 'S') {
-			Point point = { .i = i-1, .j = j-1};
-			//AddPoint(&pointArray, point);
-			return 1;
-		}
-		if(letter(letters, i+1, j+1) == 'M' && letter(letters, i-1, j-1) == 'S') {
-			Point point = { .i = i+1, .j = j+1};
-			//AddPoint(&pointArray, point);
+	if(direction == TOP_RIGHT && letters[i-1][j+1] == 'S') {
+		if((letters[i-1][j-1] == 'M' && letters[i+1][j+1] == 'S') ||
+		   (letters[i+1][j+1] == 'M' && letters[i-1][j-1] == 'S')) {
 			return 1;
 		}
 	}
-	if(direction == BOTTOM_LEFT &&	letter(letters, i+1, j-1) == 'S') {
-		if(letter(letters, i-1, j-1) == 'M' && letter(letters, i+1, j+1) == 'S') {
-			Point point = { .i = i-1, .j = j-1};
-			//AddPoint(&pointArray, point);
-			return 1;
-		}
-		if(letter(letters, i+1, j+1) == 'M' && letter(letters, i-1, j-1) == 'S') {
-			Point point = { .i = i+1, .j = j+1};
-			//AddPoint(&pointArray, point);
+	if(direction == BOTTOM_LEFT && letters[i+1][j-1] == 'S') {
+		if((letters[i-1][j-1] == 'M' && letters[i+1][j+1] == 'S') ||
+			 (letters[i+1][j+1] == 'M' && letters[i-1][j-1] == 'S')) {
 			return 1;
 		}
 	}
-	if(direction == BOTTOM_RIGHT &&	letter(letters, i+1, j+1) == 'S') {
-		if(letter(letters, i+1, j-1) == 'M' && letter(letters, i-1, j+1) == 'S') {
-			Point point = { .i = i+1, .j = j-1};
-			//AddPoint(&pointArray, point);
-			return 1;
-		}
-		if(letter(letters, i+1, j-1) == 'S' && letter(letters, i-1, j+1) == 'M') {
-			Point point = { .i = i-1, .j = j+1};
-			//AddPoint(&pointArray, point);
+	if(direction == BOTTOM_RIGHT &&	letters[i+1][j+1] == 'S') {
+		if((letters[i+1][j-1] == 'M' && letters[i-1][j+1] == 'S') ||
+			 (letters[i+1][j-1] == 'S' && letters[i-1][j+1] == 'M')) {
 			return 1;
 		}
 	}
@@ -153,42 +94,37 @@ int VerifyMas(char* letters, int i, int j, Direction direction) {
 	return 0;
 }
 
-
-int SearchMas(char* letters, int i, int j, int letterNum) {
+int SearchMas(int i, int j, int letterNum) {
 	int valid_top = (i != 0), valid_bottom = (i != HEIGHT-1);
 	int valid_left = (j != 0), valid_right = (j != WIDTH-1); 
 
 	int sum = 0;
 
-	if(valid_top && valid_left && 	 	letter(letters, i-1, j-1) == MAS[letterNum+1])	sum += VerifyMas(letters, i-1, j-1, TOP_LEFT 		);
-	if(valid_top && valid_right && 	  letter(letters, i-1, j+1) == MAS[letterNum+1])	sum += VerifyMas(letters, i-1, j+1, TOP_RIGHT 	);
-	if(valid_bottom && valid_left && 	letter(letters, i+1, j-1) == MAS[letterNum+1])	sum += VerifyMas(letters, i+1, j-1, BOTTOM_LEFT );
-	if(valid_bottom && valid_right && letter(letters, i+1, j+1) == MAS[letterNum+1]) sum += VerifyMas(letters, i+1, j+1, BOTTOM_RIGHT);
+	if(valid_top && valid_left && 	 	letters[i-1][j-1] == MAS[letterNum+1])	sum += VerifyMas(i-1, j-1, TOP_LEFT 	 );
+	if(valid_top && valid_right && 	  letters[i-1][j+1] == MAS[letterNum+1])	sum += VerifyMas(i-1, j+1, TOP_RIGHT 	 );
+	if(valid_bottom && valid_left && 	letters[i+1][j-1] == MAS[letterNum+1])	sum += VerifyMas(i+1, j-1, BOTTOM_LEFT );
+	if(valid_bottom && valid_right && letters[i+1][j+1] == MAS[letterNum+1]) 	sum += VerifyMas(i+1, j+1, BOTTOM_RIGHT);
 
 	return sum;
 }
 
 int main() {
-	char letters[HEIGHT][WIDTH];
-
 	ParseFile("input.txt", letters);
 
 	int sum = 0;
 	for(int i = 0; i < HEIGHT; i++) {
 		for(int j = 0; j < WIDTH; j++) {
-			if(letter(letters, i, j) == XMAS[0])
-				sum += SearchXmas(letters, i, j, 0, NO);
+			if(letters[i][j] == XMAS[0])
+				sum += SearchXmas(i, j, 0, NO);
 		}
 	}
 	printf("Part 1: %d\n", sum);
 
-	InitPointArray(&pointArray);
 	int sum2 = 0;
 	for(int i = 0; i < HEIGHT; i++) {
 		for(int j = 0; j < WIDTH; j++) {
-			Point point = { .i = i, .j = j };
-			if(letter(letters, i, j) == MAS[0] && !ContainsPoint(&pointArray, point))
-				sum2 += SearchMas(letters, i, j, 0);
+			if(letters[i][j] == MAS[0])
+				sum2 += SearchMas(i, j, 0);
 		}
 	}
 	printf("Part 2: %d\n", sum2/2);
